390a51203d8c5f6c709217840ea80739779fac35
Kone Gbah Ulrich Paul David <ulrichkone124@gmail.com>
2026-07-10 08:48:06 +0000

    Modification effectuer

--- app/src/main/java/com/example/happybrithday/MainActivity.kt
+++ app/src/main/java/com/example/happybrithday/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,7 +36,9 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ){
                     // CORRECTION : Appel de GreetingImage au lieu de Greeting
-                    GreetingImage(message = "Happy birthday Sam!", from = "From Emma")
+                    GreetingImage(message = stringResource(R.string.happy_birthday_text),
+                        from = stringResource(R.string.signature_text
+                    ))
                 }
             }
         }
